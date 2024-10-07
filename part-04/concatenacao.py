a3eaba53ad321d6cf5c4dff222005bfcec8c308d
Pedro <paulopedro.dev@gmail.com>
2024-10-07 10:31:46 -0400

    Adiciona os files com codigos de estudo de merge e concatenação no pandas.

---
+++ part-04/concatenacao.py
@@ -0,0 +1,34 @@
+# %%
+import pandas as pd
+import os
+# %%
+
+def import_etl(path:str):
+
+    name = path.split('/')[-1].split('.')[0]
+
+    df = (
+        pd.read_csv(path, sep=';')
+            .rename(columns={'valor': name})
+            .set_index(['cod', 'nome', 'período'])
+    )
+
+    return df
+# %%
+
+path = '../data/ipea/'
+files = os.listdir(path)
+# %%
+
+dfs = []
+for data in files:
+    dfs.append(import_etl(path+data))
+
+# %%
+dfs
+# %%
+
+
+df_consolidado = pd.concat(dfs, axis=1)
+df_consolidado
+# %%
